09575508b451633a2550cfb4d39929a36c0965de
mktb <1270874562@qq.com>
2020-05-16 00:01:49 +0800

    hhh

---
+++ src/com/mktb/hhh/Main.java
@@ -0,0 +1,12 @@
+package com.mktb.hhh;
+
+import com.mktb.hhh.ui.GameOfLifeFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new GameOfLifeFrame();
+
+    }
+}
